1c305e44e4a59e33daad3cd4a265a77e262a5b1b
Santiago Mansilla Labbé <santi755@gmail.com>
2023-11-17 16:45:17 +0100

    Add value objects generic and VO for CommentsId and PostsId

---
+++ backend/src/blog/comments/domain/value-objects/CommentsId.vo.ts
@@ -0,0 +1,3 @@
+import { UuidVO } from 'src/shared/domain/value-objects/Uuid.vo';
+
+export default class CommentsId extends UuidVO {}
---
+++ backend/src/blog/posts/domain/value-objects/PostsId.vo.ts
@@ -0,0 +1,3 @@
+import { UuidVO } from 'src/shared/domain/value-objects/Uuid.vo';
+
+export default class PostsId extends UuidVO {}
