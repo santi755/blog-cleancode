5c72f50f998d26a7d55b95fd337be361cb1b81e8
Santiago <santi755@gmail.com>
2023-11-22 20:44:45 +0100

    Remove unused imports in SearchPosts.controller.ts

--- backend/src/blog/posts/presentation/SearchPosts.controller.ts
+++ backend/src/blog/posts/presentation/SearchPosts.controller.ts
@@ -1,6 +1,6 @@
 import Posts from 'src/blog/posts/domain/entities/Posts.entity';
 import { SearchPostsService } from 'src/blog/posts/application/services/search-posts/SearchPosts.service';
-import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('posts')
 export class SearchPostsController {
