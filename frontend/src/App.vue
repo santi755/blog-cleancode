9b084b1a84e7aaac47969b3dde9bac3fd0be0ffb
Santiago <santi755@gmail.com>
2023-12-02 11:07:06 +0100

    Delete unused files and update Docker configuration

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
-    <h1>Hello world!</h1>
+    <router-view />
 </template>
-
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
