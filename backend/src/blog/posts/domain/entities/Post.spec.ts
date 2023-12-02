3bf77a92404b8dac6925941b3cd33a264c2b7998
Santiago <santi755@gmail.com>
2023-12-02 11:50:57 +0100

    Add unit tests for Post entity and CreatePosts application

---
+++ backend/src/blog/posts/domain/entities/Post.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from '@jest/globals';
+
+describe('Post', () => {
+  it('Should check if Post is created properly', async () => {
+    expect(true).toBe(true);
+  });
+});
