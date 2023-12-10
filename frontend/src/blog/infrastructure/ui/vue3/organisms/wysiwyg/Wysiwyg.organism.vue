8bb0be69ba1eabb5ed2f04de0b8e0b799cf41844
Santiago <santi755@gmail.com>
2023-12-10 02:21:07 +0100

    Add @tiptap WYSIWYG editor to the blog page

---
+++ frontend/src/blog/infrastructure/ui/vue3/organisms/wysiwyg/Wysiwyg.organism.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <EditorContent :editor="editor" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useEditor, EditorContent } from '@tiptap/vue-3';
+import StarterKit from '@tiptap/starter-kit';
+
+const editor = useEditor({
+    content: '<p>Hello World! 🌎️</p>',
+    extensions: [StarterKit]
+});
+</script>
--- frontend/src/blog/infrastructure/ui/vue3/pages/blog/Blog.page.vue
+++ frontend/src/blog/infrastructure/ui/vue3/pages/blog/Blog.page.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <ButtonAtom text="Login" appearance="outlined"></ButtonAtom>
+        <ButtonAtom text="Login" appearance="outlined" />
+        <WysiwygOrganism />
     </div>
 </template>
 
 <script lang="ts" setup>
 import ButtonAtom from '@/blog/infrastructure/ui/vue3/atoms/button/Button.atom.vue';
+import WysiwygOrganism from '@/blog/infrastructure/ui/vue3/organisms/wysiwyg/Wysiwyg.organism.vue';
 </script>
